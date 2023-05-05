992240466828d5b309ad89d46fe053b4e4b895d7
Ibrohim571 <ibrohim-01@list.ru>
2023-05-05 23:43:13 +0400

    take input values are ready

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import axios from "axios";
+
+if (process.env.NODE_ENV !== "development") {
+  axios.defaults.baseURL = "https://vercel.com/ibrohim571/bm-backend/";
+}
 
 ReactDOM.render(
   <React.StrictMode>
